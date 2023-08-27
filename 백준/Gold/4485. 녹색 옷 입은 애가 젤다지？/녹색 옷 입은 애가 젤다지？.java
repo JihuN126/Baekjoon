8d97f57a9531f1510bbba45d2f3d0f87f4fcdfb8
JihuN126 <137899326+JihuN126@users.noreply.github.com>
2023-08-27 19:40:31 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 216 ms, Memory: 18840 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,63 +1,58 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
-
-class Main {
-    static int N;
-    static int[][] Arr;
-    static int[][] Dist;
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
-    public static void main(String[] args) throws NumberFormatException, IOException {
+class Node{
+    int x,y,cost;
+    Node(int x,int y,int cost){
+        this.x=x;
+        this.y=y;
+        this.cost=cost;
+    }
+}
+public class Main {
+    static int Cave[][];
+    static int Cost[][];
+    static int[] Dx = {-1,0,1,0};
+    static int[] Dy = {0,-1,0,1};
+    static int N, Count=0;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
-        int T = 1;
-        while(true) {
+        while(true){
             N = Integer.parseInt(br.readLine());
             if(N==0) break;
-            Arr = new int[N][N];
-            Dist = new int[N][N];
-            for(int i=0;i<N;i++) {
-                st = new StringTokenizer(br.readLine());
-                for(int j=0;j<N;j++) {
-                    Arr[i][j] = Integer.parseInt(st.nextToken());
-                    Dist[i][j] = Integer.MAX_VALUE;
+            Cave = new int[N][N];
+            Cost = new int[N][N];
+            for(int i=0;i<N;i++){
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for(int j=0;j<N;j++){
+                    Cave[i][j] = Integer.parseInt(st.nextToken());
+                    Cost[i][j] = Integer.MAX_VALUE;
                 }
             }
             Dijkstra();
-            sb.append("Problem ").append(T).append(": ").append(Dist[N-1][N-1]).append("\n");
-            T ++ ;
+            sb.append("Problem ").append(++Count).append(": ").append(Cost[N-1][N-1]).append("\n");
         }
         System.out.println(sb);
     }
-    public static void Dijkstra() {
-        PriorityQueue<Node> q = new PriorityQueue<>(((o1, o2) -> o1.time-o2.time));
-        q.add(new Node(0, 0, Arr[0][0]));
-        Dist[0][0] = Arr[0][0];
-        while(!q.isEmpty()) {
-            Node Now = q.poll();
-            int x = Now.x;
-            int y= Now.y;
-            for(int i=0;i<4;i++) {
-                int Nowx = x+dx[i];
-                int Nowy = y+dy[i];
-                if(0>Nowx||0>Nowy||N<=Nowx||N<=Nowy) continue;
-                if(Dist[x][y]+Arr[Nowx][Nowy]<Dist[Nowx][Nowy]) {
-                    Dist[Nowx][Nowy] = Dist[x][y]+Arr[Nowx][Nowy];
-                    q.add(new Node(Nowx, Nowy, Dist[Nowx][Nowy]));
+    public static void Dijkstra(){
+        PriorityQueue<Node> q = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+        q.offer(new Node(0,0,Cave[0][0]));
+        Cost[0][0] = Cave[0][0];
+        while(!q.isEmpty()){
+            Node temp = q.poll();
+            int X = temp.x;
+            int Y = temp.y;
+            for(int i=0;i<4;i++){
+                int tempX = X+Dx[i];
+                int tempY = Y+Dy[i];
+                if(tempX<0 || tempY<0 || tempX>=N || tempY>=N) continue;
+                if(Cost[tempX][tempY] > Cost[X][Y] + Cave[tempX][tempY]){
+                    Cost[tempX][tempY] = Cost[X][Y] + Cave[tempX][tempY];
+                    q.offer(new Node(tempX,tempY,Cost[tempX][tempY]));
                 }
             }
         }
-    }
-}
-class Node{
-    int x, y, time;
-    public Node(int x, int y, int time) {
-        this.x = x;
-        this.y = y;
-        this.time = time;
     }
 }
